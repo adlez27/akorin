c9f4385d2b0f2fed552ed51e1a8eb8422d7a6ff7
Kiyoteru Lofe Adlez Dystopia <adlez.dysto@gmail.com>
2021-03-13 08:22:45 +0800

    Create audio settings

---
+++ Akorin/Models/Settings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akorin.Models
+{
+    class Settings
+    {
+        public string AudioDriver { get; set; }
+
+        public string AudioInputDevice { get; set; }
+
+        private int _audioInputLevel;
+        public int AudioInputLevel
+        {
+            get
+            {
+                return _audioInputLevel;
+            }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    _audioInputLevel = value;
+                else
+                    throw new ArgumentOutOfRangeException("Audio input level");
+            }
+        }
+
+        public string AudioOutputDevice { get; set; }
+
+        private int _audioOutputLevel;
+        public int AudioOutputLevel 
+        { 
+            get
+            {
+                return _audioOutputLevel;
+            } 
+            set
+            {
+                if (value >= 0 && value <= 100)
+                    _audioOutputLevel = value;
+                else
+                    throw new ArgumentOutOfRangeException("Audio output level");
+            } 
+        }
+    }
+}
